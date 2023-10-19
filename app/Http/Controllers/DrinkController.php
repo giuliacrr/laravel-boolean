fb0c1d723a9555cc87fb2c80dcb38bed826b7c4d
Pietro-Leone <leonepietro1999@gmail.com>
2023-10-19 20:03:36 +0200

    update controller - routes

--- app/Http/Controllers/DrinkController.php
+++ app/Http/Controllers/DrinkController.php
@@ -12,54 +12,8 @@ class DrinkController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $drinks = Drink::all();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Drink $drink)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Drink $drink)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Drink $drink)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Drink $drink)
-    {
-        //
+        return response()->json($drinks);
     }
 }
