1ced895b1d47df90c22fea67f4f766219e16901e
IvanoPisaniello <ivanop92@gmail.com>
2023-10-22 10:04:18 +0200

    added contact controller and migration

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get("projects", [DrinkController::class, "index"]);
+Route::post("contacts", [ContactController::class, "store"]);
