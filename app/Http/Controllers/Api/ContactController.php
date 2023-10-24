a56e74f4793ff5ee9409de11bf9ec4b9886b9d8f
giuliacrr <giulia.corrado02@gmail.com>
2023-10-24 16:19:56 +0200

    Sending email and tested with mailtrap

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Contact;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NewContact;
+use App\Mail\NewContactReceived;
 
 class ContactController extends Controller
 {
@@ -22,6 +25,12 @@ class ContactController extends Controller
         $newContact->message = $data["message"];
 
         $newContact->save();
+
+        $admins = ['ivanop92@gmail.com', 'pazigabriele@gmail.com', 'andrealinza@gmail.com'];
+        //Mail da inviare come conferma mall'utente che ha compilato il form
+        Mail::to($data['email'])->send(new NewContact($data));
+        //Mail da inviare a noi stessi come avviso che l'utente ci ha scritti.
+        Mail::to($admins)->send(new NewContactReceived($data));
 
         return response()->json([
             'message' => 'thank you for the message.'
---
+++ resources/views/emails/new_email_received.blade.php
@@ -0,0 +1,5 @@
+<h1>
+  {{ $formData["name"] }} ha provato a contattarti,
+  <br>
+  Rispondi al più presto.
+</h1>
